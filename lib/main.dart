e58ac05dbfe46ac1c31b5f5ac78a4f1b2c740230
socar-kong <kong@socar.kr>
2018-10-07 01:58:14 +0900

    fix UI

--- lib/main.dart
+++ lib/main.dart
@@ -28,13 +28,41 @@ class MyHomePage extends StatelessWidget {
           builder: (context, snapshot) {
             if (!snapshot.hasData) return const Text('Loading...');
             return new ListView.builder(
-                itemCount: snapshot.data.documents.length,
-                padding: const EdgeInsets.only(top: 10.0),
-                itemExtent: 25.0,
-                itemBuilder: (context, index) {
-                  DocumentSnapshot ds = snapshot.data.documents[index];
-                  return new Text("${ds['name']} ${ds['votes']}");
-                });
+              itemCount: snapshot.data.documents.length,
+              padding: const EdgeInsets.only(top: 10.0),
+              itemExtent: 55.0,
+              itemBuilder: (context, index) =>
+                  _buildListItem(context, snapshot.data.documents[index]),
+            );
+          }),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
+    return new ListTile(
+      key: new ValueKey(document.documentID),
+      title: new Container(
+        decoration: new BoxDecoration(
+          border: new Border.all(color: const Color(0x80000000)),
+          borderRadius: new BorderRadius.circular(5.0),
+        ),
+        padding: const EdgeInsets.all(10.0),
+        child: new Row(
+          children: <Widget>[
+            new Expanded(
+              child: new Text(document['name']),
+            ),
+            new Text(
+              document['votes'].toString(),
+            ),
+          ],
+        ),
+      ),
+      onTap: () => Firestore.instance.runTransaction((transaction) async {
+            DocumentSnapshot freshSnap =
+                await transaction.get(document.reference);
+            await transaction
+                .update(freshSnap.reference, {'votes': freshSnap['votes'] + 1});
           }),
     );
   }
